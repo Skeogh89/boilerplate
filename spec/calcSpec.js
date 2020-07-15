f4aae7a36d17a003418f4f5f0048d678d9b908fe
Stephen Keogh <stevokeogh@hotmail.com>
2020-07-15 15:53:45 +0000

    50% throught part 2

---
+++ spec/calcSpec.js
@@ -0,0 +1,10 @@
+describe("Calculator", function(){
+    describe("Addion Tests", function() {
+        it("Should return 42", function() {
+            expect(addition(20,22)).toBe(42)
+        });
+        it("Should return 26", function(){
+            expect(addition(7,19).toBe(26))
+        });
+    });
+});
